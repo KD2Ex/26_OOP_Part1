fc0cb4c6163c034310503553d382fcac3785f6a7
Серебрянский Даниил <kountdown@mail.ru>
2022-03-15 22:27:53 +0300

    done

--- Phone/main.cpp
+++ Phone/main.cpp
@@ -16,42 +16,41 @@ enum Commands {
     EXIT
 };
 
-class Phone {
-    map<string, Adress> adressBookNames;
-    map<string, Adress> adressBookPhones;
-    
+bool isPhoneNumber(string value) {
+    if (value.length() == 12) {
+        for (int i = 1; i < 12; i++) {
+            if (!isdigit(value[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+class PhoneNumber {
+    const string code = "+7";
+    char phone[10] = {};
+
+
 public:
-    void add(string name, string number) {
-        Adress adress;
-        adress.setName(name);
-        adress.setPhoneNumber(number);
-
-        adressBookNames.insert(pair<string,Adress>(name, adress));
-        adressBookPhones.insert(pair<string,Adress>(number, adress));
-        
+    void set(const char* value) {
+        for (int i = 0; i < 10; i++) {
+            phone[i] = value[i];
+        }
     }
 
-    void callOnName(string name) {
-        auto user = adressBookNames.find(name);
-
-        cout << "CALL to " << user->second.getName() << " on number " << 
-        user->second.getPhoneNumber();
+    string get() {
+        return code + phone;
     }
-
-    void callOnNumber(string phone) {
-        auto user = adressBookPhones.find(phone);
-        cout << "CALL to " << user->second.getName() << " on number " << 
-        user->second.getPhoneNumber();
-    }
-
-    
 };
 
-class Adress {
+
+class Address {
     string name;
     string phone;
-public: 
-    
+public:
+
     string getName() {
         return name;
     }
@@ -69,26 +68,81 @@ public:
     }
 };
 
-class PhoneNumber {
-    const string code = "+7";
-    char phone[10];
+
+class Phone {
+    map<string, Address> addressBookNames;
+    map<string, Address> addressBookPhones;
+    
+public:
+    void add(string name, string number) {
+        Address address;
+        address.setName(name);
+        address.setPhoneNumber(number);
+
+        addressBookNames.insert(pair<string,Address>(name, address));
+        addressBookPhones.insert(pair<string,Address>(number, address));
+        
+    }
+
+    void call(string value) {
+
+        _Rb_tree_iterator<pair<const basic_string<char>, Address>> user;
+
+        if (isPhoneNumber(value)) {
+            if (addressBookPhones.find(value) != addressBookPhones.end()) {
+                user = addressBookPhones.find(value);
+            } else {
+                cout << "Invalid phone number\n";
+                return;
+            }
+
+        } else {
+            if (addressBookNames.find(value) != addressBookNames.end()) {
+                user = addressBookNames.find(value);
+            } else {
+                cout << "Invalid phone number\n";
+                return;
+            }
+        }
+
+        cout << "CALL to " << user->second.getName() << " on number " << user->second.getPhoneNumber() << endl;
+    }
+
+    void sms(string value, string message) {
+
+        _Rb_tree_iterator<pair<const basic_string<char>, Address>> user;
+
+        if (isPhoneNumber(value)) {
+            if (addressBookPhones.find(value) != addressBookPhones.end()) {
+                user = addressBookPhones.find(value);
+            } else {
+                cout << "Invalid phone number\n";
+                return;
+            }
+
+        } else {
+            if (addressBookNames.find(value) != addressBookNames.end()) {
+                user = addressBookNames.find(value);
+            } else {
+                cout << "Invalid phone number\n";
+                return;
+            }
+        }
+
+        cout << "Message: \n" << message << "\n sent to " << user->second.getName()
+        << " on number " << user->second.getPhoneNumber() << endl;
+    }
+
 
     
-
-public:
-    void setPhoneNumber(char* value) {
-        for (int i = 0; i < 10; i++) {
-            phone[i] = value[i];
-        }
-    }
-
-    void getPhoneNumber() {
-        return 
-    }
 };
+
+
 
 int main() {
     int i;
+    Phone phone;
+
 
     map<string, int> commands;
     commands[ADD_NAME] = Commands::ADD;
@@ -96,13 +150,53 @@ int main() {
     commands[SMS_NAME] = Commands::SMS;
     commands[EXIT_NAME] = Commands::EXIT;
 
-    string inputCommand;
-    cin >> inputCommand;
+    while (true) {
+        string inputCommand;
 
-    switch(commands[inputCommand]) {
-        case Commands::ADD: {
-             
-            break;
+        cout << "Enter the command:\n";
+        cin >> inputCommand;
+
+        switch (commands[inputCommand]) {
+            case Commands::ADD: {
+                string name;
+                string phoneNumber;
+
+                cout << "Enter name and phone number\n";
+                cin >> name >> phoneNumber;
+
+                while (!isPhoneNumber(phoneNumber)) {
+                    cout << "Invalid phone number. Enter valid phone number:\n";
+                    cin >> phoneNumber;
+                }
+
+                phone.add(name, phoneNumber);
+                break;
+            }
+            case Commands::CALL: {
+                string value;
+
+                cout << "Enter the name or phone number of contact\n";
+                cin >> value;
+
+                phone.call(value);
+                break;
+            }
+            case Commands::SMS: {
+                string value;
+                string message;
+
+                cout << "Enter the name or phone number of contact and message\n";
+                cin >> value >> message;
+
+                phone.sms(value, message);
+                break;
+            }
+            case Commands::EXIT: {
+                return 0;
+            }
+            default: {
+                cout << "Wrong command.\n";
+            }
         }
     }
 }
